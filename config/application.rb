2af00209d18f2740d9bae0fb57f2b1d6e41629f8
robin vorstedt robinvorstedt@outlook.com <robinvorstedt@outlook.com>
2023-12-18 16:13:22 +0900

    test slide range

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Portfolioversion2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -30,7 +31,7 @@ module Portfolioversion2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets')
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
